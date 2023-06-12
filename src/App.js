18e83f4ab788cca078c5ac5a6218a3a411747693
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-12 15:07:49 +0200

    basic structure finished, now header

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ export const App = () => {
     <>
       <CssBaseline />
       <Header />
-      <Grid container spacing={3} style={{ width: "100%" }}></Grid>
-      <Grid item xs={12} md={4}></Grid>
-      <List />
-      <Map />
-      <PlaceDetails />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
     </>
   );
 };
---
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PlaceDetails = () => {
+  return <h1>PlaceDetails</h1>;
+};
